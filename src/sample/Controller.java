ec49dd7f61821c3137b486ddfce74252400a164b
Bigmilk120 <sprawdzany96@gmail.com>
2019-02-18 11:27:47 +0100

    First things with action listeners.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+
 public class Controller {
+    @FXML
+    Button applyDate;
+    @FXML
+    DatePicker datePicker;
+
+
+    @FXML
+    private void handleApplyButton(){
+        String data = datePicker.getEditor().getText();
+        System.out.println(data);
+
+    }
 }
