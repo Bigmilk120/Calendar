c595051635289633863a279e6c37d711af691179
Bigmilk120 <sprawdzany96@gmail.com>
2019-02-25 14:24:46 +0100

    Starting to make register page.

--- src/sample/Login.java
+++ src/sample/Login.java
@@ -8,6 +8,11 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Login {
     @FXML
     Button loginBtn;
@@ -22,20 +27,42 @@ public class Login {
     @FXML
     public void handleLoginBtn(){
         try{
-            Stage primaryStage = (Stage) loginBtn.getScene().getWindow();
-            Parent newRoot = FXMLLoader.load(getClass().getResource("sample.fxml"));
-            primaryStage.getScene().setRoot(newRoot);
             username = usernameField.getText();
             password = passwordField.getText();
+            if(isConnectionOK()) {
+                Stage primaryStage = (Stage) loginBtn.getScene().getWindow();
+                Parent newRoot = FXMLLoader.load(getClass().getResource("sample.fxml"));
+                primaryStage.getScene().setRoot(newRoot);
+            }
         }catch(Exception ex){
             System.out.println(ex);
         }
     }
+    @FXML
+    public void handleRegisterBtn(){
+        
+    }
+
+
     public static String giveUsername(){
         return username;
     }
     public static String givePassword(){
         return password;
+    }
+    public boolean isConnectionOK(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/Notes?useSSL=false&serverTimezone=UTC", username, password);
+            Statement stmt = conn.createStatement();
+            String strSelect = "select * from note";
+
+            ResultSet rset = stmt.executeQuery(strSelect);
+            return true;
+        }catch(Exception ex){
+            return false;
+        }
     }
 
 }
